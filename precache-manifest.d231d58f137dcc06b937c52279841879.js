d08fa20e99dd375f64b5b49f762cbf1ac3408591
Kundan Pawar <kpawar@ict.ag>
2020-04-14 12:28:01 +0200

    Updates

---
+++ precache-manifest.d231d58f137dcc06b937c52279841879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064c8a3d63bd786bd6eec9c779094be1",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "54a9c9425dae3763ddbe",
+    "url": "/ReactDemo/static/css/main.16efefb0.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/ReactDemo/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactDemo/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a9c9425dae3763ddbe",
+    "url": "/ReactDemo/static/js/main.5d83c1bb.chunk.js"
+  },
+  {
+    "revision": "916ae07437f16083fffc",
+    "url": "/ReactDemo/static/js/runtime-main.4e2ddba2.js"
+  }
+]);
